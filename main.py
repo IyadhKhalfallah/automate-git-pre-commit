725a7cd0c240dce1d74f819d4414b381bb99a48b
IyadhKhalfallah <iyadhkhalfallah98@gmail.com>
2023-04-04 12:38:26 +0200

    first commit

---
+++ main.py
@@ -0,0 +1,7 @@
+import boto3
+session = boto3.Session(
+    aws_access_key_id='AKIAIOSFODNN7EXAMPLE',
+    aws_secret_access_key='wJalrXUtnFEMI/K7MDENG/bPxRfiCYEXAMPLEKEY'
+)
+
+AWS_SECRET_ACCESS_KEY = '*********'
